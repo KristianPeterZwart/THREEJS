c3cee0ddbbcf3e937325b3232c5a99cb4f9177c5
Kris <kristianzwartdesign@gmail.com>
2021-04-24 17:41:01 +0200

    Update script.js

--- CHAPTER1/LESSON5-TRANSFORMOBJECTS/src/script.js
+++ CHAPTER1/LESSON5-TRANSFORMOBJECTS/src/script.js
@@ -10,27 +10,53 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 1
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = - 2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
 
 // Position
 // mesh.position.x = 0.7
 // mesh.position.y = - 0.6
 // mesh.position.z = 1
-mesh.position.set(0.7, - 0.6, 1)
-scene.add(mesh)
+// mesh.position.set(0.7, - 0.6, 1)
+// scene.add(mesh)
 
 // Scale
 // mesh.scale.x = 1
 // mesh.scale.y = 1
 // mesh.scale.z = 1
-mesh.scale.set(1, 1, 1)
+// mesh.scale.set(0, -1, 1)
 
 // Rotation
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.z = Math.PI * 0.25
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.5
+// mesh.rotation.y = Math.PI * 0.5
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(2)
@@ -51,9 +77,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1
 scene.add(camera)
+
 
 
 
